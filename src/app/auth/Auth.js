801683744e57f2e3d0671783b37dfcd546705ab5
vsys-jaydeep <35021157+vsys-jaydeep@users.noreply.github.com>
2021-04-30 11:32:28 +0530

    Removed authentication code related to Auth0 and Firebase and modified path of jwt service to auth-service

--- src/app/auth/Auth.js
+++ src/app/auth/Auth.js
@@ -1,13 +1,11 @@
 import FuseSplashScreen from '@fuse/core/FuseSplashScreen';
-import auth0Service from 'app/services/auth0Service';
-import firebaseService from 'app/services/firebaseService';
-import jwtService from 'app/services/jwtService';
+import jwtService from 'app/auth-service/jwtService';
 import { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from '@reduxjs/toolkit';
 import { hideMessage, showMessage } from 'app/store/fuse/messageSlice';
 
-import { setUserDataFirebase, setUserDataAuth0, setUserData, logoutUser } from './store/userSlice';
+import { setUserData, logoutUser } from './store/userSlice';
 
 class Auth extends Component {
 	state = {
@@ -15,12 +13,7 @@ class Auth extends Component {
 	};
 
 	componentDidMount() {
-		return Promise.all([
-			// Comment the lines which you do not use
-			this.firebaseCheck(),
-			this.auth0Check(),
-			this.jwtCheck()
-		]).then(() => {
+		return Promise.all([this.jwtCheck()]).then(() => {
 			this.setState({ waitAuthCheck: false });
 		});
 	}
@@ -68,69 +61,6 @@ class Auth extends Component {
 			return Promise.resolve();
 		});
 
-	auth0Check = () =>
-		new Promise(resolve => {
-			auth0Service.init(success => {
-				if (!success) {
-					resolve();
-				}
-			});
-
-			if (auth0Service.isAuthenticated()) {
-				this.props.showMessage({ message: 'Logging in with Auth0' });
-
-				/**
-				 * Retrieve user data from Auth0
-				 */
-				auth0Service.getUserData().then(tokenData => {
-					this.props.setUserDataAuth0(tokenData);
-
-					resolve();
-
-					this.props.showMessage({ message: 'Logged in with Auth0' });
-				});
-			} else {
-				resolve();
-			}
-
-			return Promise.resolve();
-		});
-
-	firebaseCheck = () =>
-		new Promise(resolve => {
-			firebaseService.init(success => {
-				if (!success) {
-					resolve();
-				}
-			});
-
-			firebaseService.onAuthStateChanged(authUser => {
-				if (authUser) {
-					this.props.showMessage({ message: 'Logging in with Firebase' });
-
-					/**
-					 * Retrieve user data from Firebase
-					 */
-					firebaseService.getUserData(authUser.uid).then(
-						user => {
-							this.props.setUserDataFirebase(user, authUser);
-
-							resolve();
-
-							this.props.showMessage({ message: 'Logged in with Firebase' });
-						},
-						error => {
-							resolve();
-						}
-					);
-				} else {
-					resolve();
-				}
-			});
-
-			return Promise.resolve();
-		});
-
 	render() {
 		return this.state.waitAuthCheck ? <FuseSplashScreen /> : <>{this.props.children}</>;
 	}
@@ -141,8 +71,6 @@ function mapDispatchToProps(dispatch) {
 		{
 			logout: logoutUser,
 			setUserData,
-			setUserDataAuth0,
-			setUserDataFirebase,
 			showMessage,
 			hideMessage
 		},
--- src/app/auth/store/loginSlice.js
+++ src/app/auth/store/loginSlice.js
@@ -1,7 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { showMessage } from 'app/store/fuse/messageSlice';
-import firebaseService from 'app/services/firebaseService';
-import jwtService from 'app/services/jwtService';
+import jwtService from 'app/auth-service/jwtService';
 import { setUserData } from './userSlice';
 
 export const submitLogin = ({ email, password }) => async dispatch => {
@@ -14,50 +12,6 @@ export const submitLogin = ({ email, password }) => async dispatch => {
 		})
 		.catch(errors => {
 			return dispatch(loginError(errors));
-		});
-};
-
-export const submitLoginWithFireBase = ({ email, password }) => async dispatch => {
-	if (!firebaseService.auth) {
-		console.warn("Firebase Service didn't initialize, check your configuration");
-
-		return () => false;
-	}
-	return firebaseService.auth
-		.signInWithEmailAndPassword(email, password)
-		.then(() => {
-			return dispatch(loginSuccess());
-		})
-		.catch(error => {
-			const emailErrorCodes = [
-				'auth/email-already-in-use',
-				'auth/invalid-email',
-				'auth/operation-not-allowed',
-				'auth/user-not-found',
-				'auth/user-disabled'
-			];
-			const passwordErrorCodes = ['auth/weak-password', 'auth/wrong-password'];
-			const response = [];
-
-			if (emailErrorCodes.includes(error.code)) {
-				response.push({
-					type: 'email',
-					message: error.message
-				});
-			}
-
-			if (passwordErrorCodes.includes(error.code)) {
-				response.push({
-					type: 'password',
-					message: error.message
-				});
-			}
-
-			if (error.code === 'auth/invalid-api-key') {
-				dispatch(showMessage({ message: error.message }));
-			}
-
-			return dispatch(loginError(response));
 		});
 };
 
