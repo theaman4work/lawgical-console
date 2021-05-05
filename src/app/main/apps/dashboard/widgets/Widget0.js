6cae94ce6848eb788a44cc5f5dcbbc1273f74d8a
vsys-jaydeep <35021157+vsys-jaydeep@users.noreply.github.com>
2021-05-05 17:55:22 +0530

    Added new widget to show count of 'Any Legal Services'

---
+++ src/app/main/apps/dashboard/widgets/Widget0.js
@@ -0,0 +1,29 @@
+import Icon from '@material-ui/core/Icon';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import { memo, useState } from 'react';
+
+function Widget0(props) {
+	const [currentRange, setCurrentRange] = useState(props.count);
+
+	const productName = props.product !== null ? props.product : 'Trademarks';
+
+	return (
+		<Paper className="w-full rounded-20 shadow flex flex-col justify-between">
+			<div className="flex items-center justify-between px-4">
+				<div />
+				<Icon className="text-32" style={{ color: '#0494AC' }}>
+					add_circle
+				</Icon>
+			</div>
+			<div className="text-center">
+				<Typography className="text-48 font-semibold leading-none text-blue tracking-tighter">
+					{currentRange}
+				</Typography>
+				<Typography className="text-16 text-blue-800 font-normal pb-40">{productName}</Typography>
+			</div>
+		</Paper>
+	);
+}
+
+export default memo(Widget0);
