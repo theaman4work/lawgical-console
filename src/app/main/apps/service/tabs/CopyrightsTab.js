df6d5624482b130cce0e46b21b4d71864a713b2a
vsys-dhanashree <33975624+vsys-dhanashree@users.noreply.github.com>
2022-04-25 19:07:53 +0530

    Add code to show popup for 'lserviceTransaction' list

--- src/app/main/apps/service/tabs/CopyrightsTab.js
+++ src/app/main/apps/service/tabs/CopyrightsTab.js
@@ -3,23 +3,46 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import { useTheme } from '@material-ui/core/styles';
 import Icon from '@material-ui/core/Icon';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import Slide from '@material-ui/core/Slide';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
-import { useSelector } from 'react-redux';
+import { forwardRef, useMemo, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectProductServices } from '../store/productServicesSlice';
 import { selectServices } from '../store/servicesSlice';
+import { removeServiceTransaction, selectServiceTransactions } from '../store/lserviceTransactionsSlice';
+
+const Transition = forwardRef(function Transition(props, ref) {
+	return <Slide direction="up" ref={ref} {...props} />;
+});
 
 function CopyrightsTab() {
+	const dispatch = useDispatch();
 	const theme = useTheme();
 	const productServices = useSelector(selectProductServices);
 	const subservices = useSelector(selectServices);
+	const serviceTransactions = useSelector(selectServiceTransactions);
 
 	const productServicesForCopyright = [];
 	const subServicesForCopyright = [];
+	const [dialog, setDialog] = useState({
+		open: false,
+		title: null,
+		serviceData: null,
+		records: null
+	});
 
 	// eslint-disable-next-line
 	Object.keys(productServices).map(function (keyName) {
@@ -35,9 +58,9 @@ function CopyrightsTab() {
 		}
 	});
 
-	if (productServicesForCopyright.length < 1) {
+	/*if (productServicesForCopyright.length < 1) {
 		return null;
-	}
+	}*/
 
 	const container = {
 		show: {
@@ -58,6 +81,16 @@ function CopyrightsTab() {
 		}
 	};
 
+	function handleOpenDialog(serviceData, listOfServiceTransactions) {
+		setDialog({
+			open: true,
+			title: serviceData.name,
+			serviceData,
+			records: listOfServiceTransactions
+			// ...dialogData
+		});
+	}
+
 	function buttonStatus(course) {
 		switch (course.activeStep) {
 			case course.totalSteps:
@@ -70,7 +103,117 @@ function CopyrightsTab() {
 	}
 
 	return (
-		<div className="flex flex-col flex-auto flex-shrink-0 w-full">
+		<>
+			<div>
+				{useMemo(() => {
+					function handleCloseDialog() {
+						setDialog({
+							...dialog,
+							open: false
+						});
+					}
+
+					return (
+						<Dialog
+							classes={{
+								paper: 'm-24'
+							}}
+							open={dialog.open}
+							onClose={handleCloseDialog}
+							aria-labelledby="service-applications-list"
+							TransitionComponent={Transition}
+							fullWidth
+							maxWidth="xs"
+						>
+							<AppBar position="static" elevation={0} className="items-center">
+								<Typography variant="h6" color="inherit" className="p-16 items-center">
+									{dialog.title}
+								</Typography>
+							</AppBar>
+							<DialogContent className="w-full">
+								<List component="nav" className="w-full p-0 pt-0" autoFocus>
+									{dialog.records &&
+										// eslint-disable-next-line
+									dialog.records.map((serviceTransaction, index) => {
+											const url = `/apps/services/steps/copyrights/${dialog.serviceData.id}`;
+											if (serviceTransaction.status === 'ACTIVE') {
+												return (
+													<ListItem
+														key={serviceTransaction.id}
+														className="pl-12"
+														button
+														divider
+														component={Link}
+														to={`${url}/${serviceTransaction.id}`}
+													>
+														<ListItemText
+															key={serviceTransaction.id + 1}
+															primary={
+																serviceTransaction.lablelByUser !== null
+																	? serviceTransaction.lablelByUser
+																	: serviceTransaction.sysGenName
+															}
+														/>
+														<ListItemSecondaryAction>
+															<IconButton
+																edge="end"
+																aria-label="deleteTransaction"
+																onClick={ev => {
+																	ev.stopPropagation();
+																	const removeIds = [serviceTransaction.id];
+																	dispatch(removeServiceTransaction(removeIds));
+																	const listOfServiceTransactionsUpdated = serviceTransactions.filter(
+																		rec =>
+																			rec.servicesId === dialog.serviceData.id &&
+																			rec.status === 'ACTIVE' &&
+																			rec.id !== serviceTransaction.id
+																	);
+																	handleOpenDialog(
+																		dialog.serviceData,
+																		listOfServiceTransactionsUpdated
+																	);
+																}}
+															>
+																<DeleteIcon />
+															</IconButton>
+														</ListItemSecondaryAction>
+													</ListItem>
+												);
+												// eslint-disable-next-line
+											} else {
+												return null;
+											}
+										})}
+								</List>
+							</DialogContent>
+							<DialogActions className="p-16">
+								<Button
+									color="primary"
+									variant="contained"
+									size="medium"
+									aria-label="closePopup"
+									component={Link}
+									to={
+										dialog.serviceData && `/apps/services/steps/copyrights/${dialog.serviceData.id}`
+									}
+								>
+									Add New
+								</Button>
+								<Button
+									onClick={handleCloseDialog}
+									color="primary"
+									variant="contained"
+									size="medium"
+									aria-label="closePopup"
+								>
+									Close
+								</Button>
+							</DialogActions>
+						</Dialog>
+					);
+				}, [dialog, dispatch, serviceTransactions])}
+			</div>
+			<div className="flex flex-col flex-auto flex-shrink-0 w-full">
 			{productServicesForCopyright &&
 				(productServicesForCopyright.length > 0 ? (
 					<motion.div className="flex flex-wrap py-24" variants={container} initial="hidden" animate="show">
@@ -108,21 +251,52 @@ function CopyrightsTab() {
 															subService.productLserviceId === productService.id &&
 															subService.status === 'ACTIVE'
 														) {
-															return (
-																<ListItem
-																	key={i}
-																	// onClick={() => handleOpenDialog(article)}
-																	className="pl-12"
-																	button
-																	component={Link}
-																	to={url}
-																>
-																	<ListItemIcon className="min-w-40">
-																		<Icon className="text-20">import_contacts</Icon>
-																	</ListItemIcon>
-																	<ListItemText primary={subService.name} />
-																</ListItem>
+															const listOfServiceTransactions = serviceTransactions.filter(
+																rec =>
+																	rec.servicesId === subService.id &&
+																	rec.status === 'ACTIVE'
 															);
+
+															if (listOfServiceTransactions.length > 0) {
+																return (
+																	<ListItem
+																		key={subService.id}
+																		onClick={() =>
+																			handleOpenDialog(
+																				subService,
+																				listOfServiceTransactions
+																			)
+																		}
+																		className="pl-12"
+																		button
+																	>
+																		<ListItemIcon className="min-w-40">
+																			<Icon className="text-20">
+																				import_contacts
+																			</Icon>
+																		</ListItemIcon>
+																		<ListItemText primary={subService.name} />
+																	</ListItem>
+																);
+																// eslint-disable-next-line
+															} else {
+																return (
+																	<ListItem
+																		key={subService.id}
+																		className="pl-12"
+																		button
+																		component={Link}
+																		to={url}
+																	>
+																		<ListItemIcon className="min-w-40">
+																			<Icon className="text-20">
+																				import_contacts
+																			</Icon>
+																		</ListItemIcon>
+																		<ListItemText primary={subService.name} />
+																	</ListItem>
+																);
+															}
 														}
 													})
 												}
@@ -142,11 +316,12 @@ function CopyrightsTab() {
 				) : (
 					<div className="flex flex-1 items-center justify-center">
 						<Typography color="textSecondary" className="text-24 my-24">
-							No Trademark services found!
+							No Copyright services found!
 						</Typography>
 					</div>
 				))}
-		</div>
+			</div>
+		</>
 	);
 }
 
